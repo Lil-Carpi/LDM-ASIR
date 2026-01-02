b9d16cd92ba70f2477fa1e4df43d42f2f3106ef3
Lil-Carpi <duartesebas007@gmail.com>
2026-01-02 16:43:42 +0100

    Separado los contenidos de html, css y javascript en diferentes directorios \ Creado primera actividad de JavaScript

---
+++ JavaScript/A01-SAD/assets/js/index.js
@@ -0,0 +1,15 @@
+const root = document.documentElement;
+const toggleBtn = document.getElementById("theme-toggle");
+
+const savedTheme = localStorage.getItem("theme");
+if (savedTheme) {
+  root.dataset.theme = savedTheme;
+}
+
+toggleBtn.addEventListener("click", () => {
+  const current = root.dataset.theme;
+  const next = current === "dark" ? "light" : "dark";
+
+  root.dataset.theme = next;
+  localStorage.setItem("theme", next);
+});
